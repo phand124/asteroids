7ff8f6188a83abb1539391a85971c7be6112b6ff
future_potato <phillip@futurepotato.net>
2025-11-29 12:07:56 -0900

    Constants module added, imported screen size variables.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
